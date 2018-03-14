6e2deac75c720f8003b9fbd699163f81a32ec21d
SallyKa <37332949+SallyKa@users.noreply.github.com>
2018-03-14 08:19:43 -0400

    Update team4.py

--- team4.py
+++ team4.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Salveah' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'We Have Not Decided on the Name Yet'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
